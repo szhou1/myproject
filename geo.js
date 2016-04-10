ed342ccb0880849f539e28772c5b647a081bb813
szhou1 <zhousteve3@gmail.com>
2016-04-10 03:33:41 -0700

    Added geocoding for searched location

--- geo.js
+++ geo.js
@@ -1,14 +1,73 @@
-var x = document.getElementById("location");
-function getLocation() {
-  console.log("getLocation() start");
-    if (navigator.geolocation) {
-      console.log("getting current position");
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+function geocodeAddress() {
+  var searchterm = document.getElementById("searchquery").value;
+  console.log("geocodeAddress() start: " + searchterm);
+
+  sendGeoRequest(searchterm, function(req){
+    console.log(req.responseText);
+
+
+    var response = JSON.parse(req.responseText);
+    // console.log(response.results[0].geometry.location.lat);
+    // console.log(response.results[0].geometry.location.lng);
+    var ll = {};
+     ll = response.results[0].geometry.location;
+
+    //  console.log("LAT: " + ll);
+     var location = document.getElementById("location");
+     location.innerHTML = ll.lat + ", " + ll.lng ;
+  });
+
 }
+
+function sendGeoRequest(address, process) {
+  // console.log("sendRequest() start");
+  var urlbase = "https://maps.googleapis.com/maps/api/geocode/json";
+  var key = "AIzaSyDAmUaj9BXpUzsj9nPhwMMPwiUC9M6DMG0";
+  var url = urlbase + "?address=" + address + '&key=' + key;
+  // console.log(url);
+
+  var req = new XMLHttpRequest();
+  // req.addEventListener("loadend", loadEndListener);
+  req.onreadystatechange = function (aEvt) {
+    if (req.readyState == 4) {
+       if(req.status == 200)
+        process(this);
+       else
+        alert("Error loading page\n");
+    }
+  };
+
+  req.open('GET', url, true);
+  req.setRequestHeader('Content-Type', 'text/plain');
+  req.send();
+
+  // console.log("sendRequest() end");
+}
+
+
+function geoFindMe() {
+  console.log("geoFindMe() start");
+
+    navigator.geolocation.watchPosition(function(position) {
+      console.log("i'm tracking you!");
+      if (navigator.geolocation) {
+          console.log("getting current position");
+          navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+          document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
+      }
+    },
+    function (error) {
+      if (error.code == error.PERMISSION_DENIED)
+          console.log("you denied me :-(");
+          document.getElementById("location").innerHTML = "geolocation blocked";
+    });
+
+
+
+}
+
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
+    ocument.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
 }
